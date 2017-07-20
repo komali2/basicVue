b07e3f41f21ac5c7e9064ceafe0ff74683024f17
Caleb Rogers <rogersjcaleb@gmail.com>
2017-07-20 16:16:20 -0700

    Get debounce working

--- basicVue/src/components/Searchbar.vue
+++ basicVue/src/components/Searchbar.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="searchbar">
-    Searchbar
+
+  <input v-model="searchterm" placeholder="Search...">
+  <p>{{help_text}}</p>
   </div>
 </template>
 
 <script>
+import _ from 'underscore';
+
 export default {
-  name: 'searcbhar',
+  name: 'searchbar',
   data() {
     return {
-
+      help_text: 'Insert a search term!',
+      searchterm: '',
     };
+  },
+  watch: {
+    searchterm: function searchterm(term) {
+      this.help_text = 'Waiting for you to finish typing...';
+      this.search(term);
+    },
+  },
+  methods: {
+    search: _.debounce(
+      function search(term) {
+        console.log(term);
+      },
+      // This is the number of milliseconds we wait for the
+      // user to stop typing.
+      500,
+    ),
   },
 };
 </script>
